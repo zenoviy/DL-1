6a1c3990f8a75c28f48f6acc7fb6dcae76fcc1fc
Zenoviy <zenoviy.shulba@gmail.com>
2020-07-15 21:36:04 +0200

    js started

---
+++ js/base/js/main.js
@@ -0,0 +1,64 @@
+/*function geoPosition(){
+        navigator.geolocation.getCurrentPosition(showPosition);
+    }
+    function showPosition(position){
+        console.log(position.coords.latitude, position.coords.longitude);
+    }
+    geoPosition()
+*/
+
+
+//alert("World");
+
+///  Прості типи
+// String  - текст
+// Number  - числа  0-9  -Infiniy   +Infinity
+// Boolean - true false
+// Object  - об'єкти   властивості   методи/прототипи
+// Undefined - спеціальний тип
+// Null     - відсутність об'єкта
+// Symbol  - (ES2015) - позначення об'єктів
+
+// NaN      - Not a number
+
+
+
+// Об'єкти Прототипи
+
+// Function
+// Array
+
+var myStringVariable = "Text";
+
+/*console.log(myStringVariable, typeof myStringVariable, 1)
+myStringVariable = 55;
+
+
+console.log(myStringVariable,typeof myStringVariable, 2)
+
+myStringVariable = false;
+console.log(myStringVariable,typeof myStringVariable, 3)*/
+
+
+const myConstant = "Lviv";
+/*myConstant = 6;
+console.log(myConstant)*/
+
+if(true) {
+    let myLocalVariables = 4;
+    console.log(myLocalVariables);
+}
+console.log(myLocalVariables);/**/
+
+/*var a = 5;
+function foo(){
+    a = 10;
+    console.log(a)
+}*/
+
+
+
+
+
+foo()
+console.log(a)
